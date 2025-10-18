7cfefbeb1374b3549bc8a4eaa92b098501d50867
annette.graham <annette.graham@sky.uk>
2025-10-18 17:36:32 +0100

    Handle fetch

--- src/components/app.test.tsx
+++ src/components/app.test.tsx
@@ -3,6 +3,14 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
+  beforeAll(() => {
+    global.fetch = vi.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve([]),
+      } as any)
+    );
+  });
+
   it('should render without crashing', () => {
     const { container } = render(<App />);
     expect(container).toBeInTheDocument();
